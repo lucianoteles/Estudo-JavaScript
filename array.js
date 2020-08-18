eceb1adbc24a8e9269615b26cbdf43d90c3519f7
LucianoTeles <50302298+LucianoTeles@users.noreply.github.com>
2020-08-18 18:19:56 -0300

    Modified

---
+++ array.js
@@ -0,0 +1,8 @@
+var nomes = []; // avisa o JavaScript que a variável é um array
+nomes.push("Felipe"); // Insere o nome no final do array
+nomes.push("Douglas");
+nomes.push("João");
+nomes.pop(); // Exclui o último valor do array
+nomes.push("Mariana");
+console.log(nomes);
+// → ["Felipe", "Douglas", "Mariana"]
